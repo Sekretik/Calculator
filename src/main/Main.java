d1f9dddf22c3f5896cb87a8a84e4b222fc9865a8
Sekretik <90332361+Sekretik@users.noreply.github.com>
2023-03-21 00:43:48 +0500

    Console input

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,14 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
+        Scanner scanner = new Scanner(System.in);
 
-        String expression = "1 - 2";
+        String expression = scanner.nextLine();
 
         try {
             System.out.println(calculator.calculate(expression));
